9d13981a09c40b07c4cf23be4043997081b2a712
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:51 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "storage_object_replications" {
-  description = "All storage_object_replication resources"
-  value       = azurerm_storage_object_replication.storage_object_replications
-}
 output "storage_object_replications_destination_object_replication_id" {
-  description = "List of destination_object_replication_id values across all storage_object_replications"
-  value       = [for k, v in azurerm_storage_object_replication.storage_object_replications : v.destination_object_replication_id]
+  description = "Map of destination_object_replication_id values across all storage_object_replications, keyed the same as var.storage_object_replications"
+  value       = { for k, v in azurerm_storage_object_replication.storage_object_replications : k => v.destination_object_replication_id }
 }
 output "storage_object_replications_destination_storage_account_id" {
-  description = "List of destination_storage_account_id values across all storage_object_replications"
-  value       = [for k, v in azurerm_storage_object_replication.storage_object_replications : v.destination_storage_account_id]
+  description = "Map of destination_storage_account_id values across all storage_object_replications, keyed the same as var.storage_object_replications"
+  value       = { for k, v in azurerm_storage_object_replication.storage_object_replications : k => v.destination_storage_account_id }
 }
 output "storage_object_replications_metrics_enabled" {
-  description = "List of metrics_enabled values across all storage_object_replications"
-  value       = [for k, v in azurerm_storage_object_replication.storage_object_replications : v.metrics_enabled]
+  description = "Map of metrics_enabled values across all storage_object_replications, keyed the same as var.storage_object_replications"
+  value       = { for k, v in azurerm_storage_object_replication.storage_object_replications : k => v.metrics_enabled }
 }
 output "storage_object_replications_rules" {
-  description = "List of rules values across all storage_object_replications"
-  value       = [for k, v in azurerm_storage_object_replication.storage_object_replications : v.rules]
+  description = "Map of rules values across all storage_object_replications, keyed the same as var.storage_object_replications"
+  value       = { for k, v in azurerm_storage_object_replication.storage_object_replications : k => v.rules }
 }
 output "storage_object_replications_source_object_replication_id" {
-  description = "List of source_object_replication_id values across all storage_object_replications"
-  value       = [for k, v in azurerm_storage_object_replication.storage_object_replications : v.source_object_replication_id]
+  description = "Map of source_object_replication_id values across all storage_object_replications, keyed the same as var.storage_object_replications"
+  value       = { for k, v in azurerm_storage_object_replication.storage_object_replications : k => v.source_object_replication_id }
 }
 output "storage_object_replications_source_storage_account_id" {
-  description = "List of source_storage_account_id values across all storage_object_replications"
-  value       = [for k, v in azurerm_storage_object_replication.storage_object_replications : v.source_storage_account_id]
+  description = "Map of source_storage_account_id values across all storage_object_replications, keyed the same as var.storage_object_replications"
+  value       = { for k, v in azurerm_storage_object_replication.storage_object_replications : k => v.source_storage_account_id }
 }
 
