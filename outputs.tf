2f91cb663b40e8bb5588e3ea413e714f9e0b8cd2
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:51:22 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
 output "storage_object_replications_id" {
   description = "Map of id values across all storage_object_replications, keyed the same as var.storage_object_replications"
-  value       = { for k, v in azurerm_storage_object_replication.storage_object_replications : k => v.id }
+  value       = { for k, v in azurerm_storage_object_replication.storage_object_replications : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "storage_object_replications_destination_object_replication_id" {
   description = "Map of destination_object_replication_id values across all storage_object_replications, keyed the same as var.storage_object_replications"
-  value       = { for k, v in azurerm_storage_object_replication.storage_object_replications : k => v.destination_object_replication_id }
+  value       = { for k, v in azurerm_storage_object_replication.storage_object_replications : k => v.destination_object_replication_id if v.destination_object_replication_id != null && length(v.destination_object_replication_id) > 0 }
 }
 output "storage_object_replications_destination_storage_account_id" {
   description = "Map of destination_storage_account_id values across all storage_object_replications, keyed the same as var.storage_object_replications"
-  value       = { for k, v in azurerm_storage_object_replication.storage_object_replications : k => v.destination_storage_account_id }
+  value       = { for k, v in azurerm_storage_object_replication.storage_object_replications : k => v.destination_storage_account_id if v.destination_storage_account_id != null && length(v.destination_storage_account_id) > 0 }
 }
 output "storage_object_replications_metrics_enabled" {
   description = "Map of metrics_enabled values across all storage_object_replications, keyed the same as var.storage_object_replications"
-  value       = { for k, v in azurerm_storage_object_replication.storage_object_replications : k => v.metrics_enabled }
+  value       = { for k, v in azurerm_storage_object_replication.storage_object_replications : k => v.metrics_enabled if v.metrics_enabled != null }
 }
 output "storage_object_replications_rules" {
   description = "Map of rules values across all storage_object_replications, keyed the same as var.storage_object_replications"
-  value       = { for k, v in azurerm_storage_object_replication.storage_object_replications : k => v.rules }
+  value       = { for k, v in azurerm_storage_object_replication.storage_object_replications : k => v.rules if v.rules != null && length(v.rules) > 0 }
 }
 output "storage_object_replications_source_object_replication_id" {
   description = "Map of source_object_replication_id values across all storage_object_replications, keyed the same as var.storage_object_replications"
-  value       = { for k, v in azurerm_storage_object_replication.storage_object_replications : k => v.source_object_replication_id }
+  value       = { for k, v in azurerm_storage_object_replication.storage_object_replications : k => v.source_object_replication_id if v.source_object_replication_id != null && length(v.source_object_replication_id) > 0 }
 }
 output "storage_object_replications_source_storage_account_id" {
   description = "Map of source_storage_account_id values across all storage_object_replications, keyed the same as var.storage_object_replications"
-  value       = { for k, v in azurerm_storage_object_replication.storage_object_replications : k => v.source_storage_account_id }
+  value       = { for k, v in azurerm_storage_object_replication.storage_object_replications : k => v.source_storage_account_id if v.source_storage_account_id != null && length(v.source_storage_account_id) > 0 }
 }
 
